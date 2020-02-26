2d11ca89ad786667362dfea49e9bb4db8ca5b855
Skyler Mäntysaari <sm@samip.fi>
2020-02-26 18:04:14 +0200

    Added the static file url.

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from . import views
@@ -7,7 +8,7 @@ urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
     path("package/", include('app.packages.urls')),
     path("admin/", admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
